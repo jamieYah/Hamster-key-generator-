8fba15b3df46509397a1930fb84bfa1ae3fa7dc3
jamieYah <68698661+jamieYah@users.noreply.github.com>
2024-09-19 20:24:51 +0100

    Update script.js

--- script.js
+++ script.js
@@ -168,6 +168,20 @@ document.addEventListener('DOMContentLoaded', () => {
             promoId: 'eb518c4b-e448-4065-9d33-06f3039f0fcb',
             timing: 20000, // 30 seconds
             attempts: 30,
+        },
+        19: {
+            name: 'Among Water',
+            appToken: 'daab8f83-8ea2-4ad0-8dd5-d33363129640',
+            promoId: 'daab8f83-8ea2-4ad0-8dd5-d33363129640',
+            timing: 20000, // 30 seconds
+            attempts: 30,
+        },
+        20: {
+            name: 'Factory World',
+            appToken: 'd02fc404-8985-4305-87d8-32bd4e66bb16',
+            promoId: 'd02fc404-8985-4305-87d8-32bd4e66bb16',
+            timing: 20000, // 30 seconds
+            attempts: 30,
         }
     };
 
